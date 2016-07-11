753396e5e40a7cc70b86bc27db56b1579ce9aec6
focolare <genfest.by@gmail.com>
2016-07-11 23:06:14 +0300

    Create config

---
+++ js/config.js
@@ -0,0 +1,15 @@
+configReactDriveCms = {
+    author: 'Your Author Name',
+    /**
+     * Add the Id for the Google Sheets Dashboard file
+     */
+    dashboardId: '1wxIdYBuQZgJ_MOIZP3DM8gKnvOPVqqxsXW8vY78pq24',
+    /**
+     * Add the Url Id for the Google Web App Script to forward email from the Contact Form
+     */
+    sendMailUrlId: 'THE-URL-ID-TO-YOUR-GOOGLE-APPS-SCRIPT-PUBLISHED-WEB-APP',
+    /**
+     * Add your Disqus shortname
+     */
+    shortname: 'your-disqus-shortname'
+};
